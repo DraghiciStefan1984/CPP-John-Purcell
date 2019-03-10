79c34af780c352d24c8cd219adbcbfb319858783
StefanDraghici1984 <draghicistefan1984@gmail.com>
2019-03-10 21:45:05 +0200

    classes

--- CPP/CPP/Person.cpp
+++ CPP/CPP/Person.cpp
@@ -1,14 +1,32 @@
 #include "pch.h"
 #include "Person.h"
+#include <sstream>
 
-
-Person::Person()
+Person::Person() : name("undefined"), age(0)
 {
 }
 
+Person::Person(string name)
+{
+	this->name = name;
+}
+
+/*
+Person::Person(string name, int age)
+{
+	this->name = name;
+	this->age = age;
+}
+*/
+
+//using parameter list to constructor
+Person::Person(string name, int age) : name(name), age(age)
+{
+}
 
 Person::~Person()
 {
+	cout << "Person Destructor" << endl;
 }
 
 void Person::SetName(string name)
@@ -21,7 +39,19 @@ string Person::GetName()
 	return name;
 }
 
+void Person::SetAge(int age)
+{
+	this->age = age;
+}
+
+int Person::GetAge()
+{
+	return this->age;
+}
+
 string Person::ToString()
 {
-	return "Person name is " + name;
+	stringstream ss;
+	ss << "Name: " << name << "; Age: " << age << endl;
+	return ss.str();
 }
--- CPP/CPP/Person.h
+++ CPP/CPP/Person.h
@@ -6,12 +6,17 @@ class Person
 {
 private:
 	string name;
+	int age;
 
 public:
 	Person();
+	Person(string name);
+	Person(string name, int age);
 	~Person();
 	void SetName(string name);
 	string GetName();
+	void SetAge(int age);
+	int GetAge();
 	string ToString();
 };
 
