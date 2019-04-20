c2b527be09fc1514501714489edb387e87d19b6a
StefanDraghici1984 <draghicistefan1984@gmail.com>
2019-04-20 17:45:16 +0300

    fractal start

---
+++ Fractal/Fractal/Bitmap.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "Bitmap.h"
+
+Bitmap::Bitmap(int width, int height) : m_width(width), m_height(height), m_pPixels(new uint8_t[width*height*3]){}
+
+Bitmap::~Bitmap(){}
+
+bool Bitmap::write(string filename)
+{
+	return false;
+}
+
+void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue)
+{
+}
---
+++ Fractal/Fractal/BitmapFileHeader.h
@@ -0,0 +1,19 @@
+#pragma once
+#pragma pack(2)
+
+#ifndef BITMAPFILEHEADER_H_
+#define BITMAPFILEHEADER_H_
+
+#include <cstdint>
+
+using namespace std;
+
+struct BitmapFileHeader
+{
+	char header[2]{ 'B', 'M' };
+	int32_t fileSize;
+	int32_t reserved{ 0 };
+	int32_t dataOffset;
+};
+
+#endif // !BITMAPFILEHEADER_H_
